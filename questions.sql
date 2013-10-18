76e78861dc557f86366d82ce834df9d9c7b920bb
GZhenin <gregory.zhenin@gmail.com>
2013-10-18 11:22:21 -0400

    First 5 questions done.

--- questions.sql
+++ questions.sql
@@ -4,32 +4,60 @@
 # Select all the user names from the users table. 
 SELECT name FROM users;
 
-#Select all the user names whose name starts with the letter “b”. 
+#Select all the user names whose name starts with the letter “b”.
 
-#How many users are there? 
+SELECT name FROM users WHERE name LIKE("b%"); 
+
+#How many users are there?
+
+select count(name) from users;
 
 #How many statuses by a certain user? 
 
+select count(s.status) from statuses s inner join users u on u.id = s.user_id where u.name = "babyjojo107";
+
 #Return the user name column, and all the statuses by that user. 
+
+select u.name, s.status from statuses s inner join users u on u.id = s.user_id where u.name = "babyjojo107";
 
 #Return all users who have at least 1 status that has a geocode. (Hint, you’ll want somewhere your WHERE clause “geocode IS NOT NULL”). 
 
+select distinct u.name from users u inner join statuses s on u.id = s.user_id where geocode is not null;
+
 #Return all user names who have written more than one status. 
+
+
 
 #Return all user names who have written more than one status with the tag “food.” 
 
+
+
 #Return user names, their statuses, and their tags in one big join. 
+
+
 
 #What is the most recent status? 
 
+
+
 #What is the most popular tag? (Hint, count how many statuses their are by tag). 
+
+
 
 #What is the most popular user? 
 
+
+
 #Give me ALL statuses by all users, with their tags, even the ones that don’t necessarily have a tag. 
+
+
 
 #Extra Credit
 #What is the most popular word? (you may need the help of ruby)
 
+
+
 #What are some other kinds of queries that are useful? 
+
+
 
