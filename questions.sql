3dc46e599d8c6a3e4d847e980afbf40679566953
squit <michael.squitieri@gmail.com>
2013-10-18 00:34:25 -0400

    finished sql assignment

--- questions.sql
+++ questions.sql
@@ -18,12 +18,13 @@ SELECT COUNT(*) FROM statuses where user_id=USER_ID
 SELECT name, status FROM users inner join statuses on users.id = statuses.user_id where users.id=USER_ID
 
 #Return all users who have at least 1 status that has a geocode. (Hint, you’ll want somewhere your WHERE clause “geocode IS NOT NULL”). 
-SELECT name FROM users left join statuses on users.id = statuses.user_id WHERE geocode IS NOT NULL;
-# But, this does return duplicates!
+SELECT DISTINCT name FROM users left join statuses on users.id = statuses.user_id WHERE geocode IS NOT NULL;
 
-#Return all user names who have written more than one status. 
+#Return all user names who have written more than one status.
+SELECT name FROM users u left join statuses s on u.id = s.user_id group by s.user_id HAVING count(*) > 1;
 
-#Return all user names who have written more than one status with the tag “food.” 
+#Return all user names who have written more than one status with the tag “food.”
+SELECT name FROM statuses_tags st left join statuses s on st.status_id = s.id left join users u on s.user_id=u.id group by u.name HAVING tag_id=1 AND count(*)>1;
 
 #Return user names, their statuses, and their tags in one big join. 
 SELECT name, status, tag FROM users left join statuses on users.id = statuses.user_id left join statuses_tags on statuses.id = statuses_tags.status_id left join tags on statuses_tags.tag_id = tags.id;   
@@ -42,5 +43,6 @@ SELECT status, tag FROM statuses s left join statuses_tags st on s.id = st.statu
 
 #Extra Credit
 #What is the most popular word? (you may need the help of ruby)
+The most popular word is "obama" with 1110 occurances. Take a look at concordance.rb for the implementation.
 
 #What are some other kinds of queries that are useful? 
