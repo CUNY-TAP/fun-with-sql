7f9d5b6db62fc2f2fc6183afa9c6118a67e39007
lhuang42 <lhuang42@gmail.com>
2013-10-18 15:26:06 -0400

    Updated some of my answers to be a little more specific.

--- questions.sql
+++ questions.sql
@@ -12,16 +12,27 @@ SELECT name FROM users WHERE name LIKE "b%";
 SELECT COUNT(id) FROM users;
 
 #How many posts by a certain user?
-SELECT COUNT(status) FROM users JOIN statuses ON users.id = statuses.user_id WHERE users.name = "name_here"; 
+SELECT COUNT(status) 
+FROM 
+users JOIN statuses ON users.id = statuses.user_id 
+WHERE users.name = "name_here"; 
 
 #Return the user name column, and all the status by that user.
-SELECT name, status FROM users JOIN statuses ON users.id = statuses.user_id WHERE users.name = "name_here";
+SELECT name, status 
+FROM 
+users JOIN statuses ON users.id = statuses.user_id 
+WHERE users.name = "name_here";
 
 #Return all users who have at least 1 status that has a geocode. (Hint, you’ll want somewhere your WHERE clause “geocode IS NOT NULL”). 
-SELECT DISTINCT name FROM users JOIN statuses ON users.id = statuses.user_id WHERE statuses.geocode IS NOT NULL;
+SELECT DISTINCT name 
+FROM 
+users JOIN statuses ON users.id = statuses.user_id 
+WHERE statuses.geocode IS NOT NULL;
 
 #Return all user names who have written more than one status. 
-SELECT DISTINCT name FROM users JOIN statuses ON users.id = statuses.user_id GROUP BY user_id HAVING COUNT(status) > 1;
+SELECT DISTINCT name 
+FROM users JOIN statuses ON users.id = statuses.user_id 
+GROUP BY user_id HAVING COUNT(status) > 1;
 
 #Return all user names who have written more than one status with the tag “food.” 
 SELECT name, status
@@ -39,20 +50,28 @@ JOIN statuses_tags ON statuses.id = statuses_tags.status_id
 JOIN tags ON statuses_tags.tag_id = tags.id;
 
 #What is the most recent status? 
-SELECT status FROM statuses ORDER BY created_at DESC LIMIT 1;
+SELECT status FROM statuses 
+ORDER BY created_at DESC LIMIT 1;
 
 #What is the most popular tag? (Hint, count how many statuses their are by tag). 
-SELECT DISTINCT tag
-FROM
+SELECT tag FROM
 tags JOIN statuses_tags ON tags.id = statuses_tags.tag_id
-GROUP BY tag HAVING COUNT(status_id);
+GROUP BY tag HAVING COUNT(status_id) = (
+	SELECT COUNT(status_id)
+	FROM
+	tags JOIN statuses_tags ON tags.id = statuses_tags.tag_id
+	GROUP BY tag ORDER BY COUNT(status_id) DESC LIMIT 1
+);
 
 #What is the most popular user?
-SELECT name, COUNT(status)
-FROM
+SELECT name FROM
 users JOIN statuses ON users.id = statuses.user_id
-GROUP BY user_id
-ORDER BY COUNT(status) DESC;
+GROUP BY user_id HAVING COUNT(status) = (
+	SELECT COUNT(status)
+	FROM
+	users JOIN statuses ON users.id = statuses.user_id
+	GROUP BY user_id ORDER BY COUNT(status) DESC LIMIT 1
+);
 
 #Give me ALL statuses by all users, with their tags, even the ones that don’t necessarily have a tag. 
 SELECT name, status, tag
