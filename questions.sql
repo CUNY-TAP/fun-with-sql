c6cbac954abdc877466d1c3a8a7a9420839c4602
Gregory Zhenin <gregory.zhenin@gmail.com>
2013-10-18 18:58:28 -0400

    Finished the questions, didn't do the extra credit :(

--- questions.sql
+++ questions.sql
@@ -26,31 +26,31 @@ select distinct u.name from users u inner join statuses s on u.id = s.user_id wh
 
 #Return all user names who have written more than one status. 
 
-
+select distinct u.name from users u inner join statuses s on u.id = s.user_id group by u.name having count(s.status)>1;
 
 #Return all user names who have written more than one status with the tag “food.” 
 
-
+select u.name from users u inner join statuses s on u.id = s.user_id left join statuses_tags st on s.id=st.status_id left join tags t on st.tag_id = t.id group by u.name having count(s.status)>1 and t.tag="food";
 
 #Return user names, their statuses, and their tags in one big join. 
 
-
+select u.name from users u inner join statuses s on u.id = s.user_id left join statuses_tags st on s.id=st.status_id left join tags t on st.tag_id = t.id;
 
 #What is the most recent status? 
 
-
+select status from statuses where created_at = (select max(created_at) from statuses);
 
 #What is the most popular tag? (Hint, count how many statuses their are by tag). 
 
-
+select t.tag from tags t inner join statuses_tags st on st.tag_id = t.id inner join statuses s on s.id = st.status_id group by t.tag order by count(st.tag_id) desc limit 1;
 
 #What is the most popular user? 
 
-
+select u.name from users u inner join statuses s on u.id = s.user_id group by u.name order by count(s.user_id) desc limit 1;
 
 #Give me ALL statuses by all users, with their tags, even the ones that don’t necessarily have a tag. 
 
-
+select u.name, s.status, t.tag from users u inner join statuses s on u.id = s.user_id left join statuses_tags st on s.id=st.status_id left join tags t on st.tag_id = t.id;
 
 #Extra Credit
 #What is the most popular word? (you may need the help of ruby)
